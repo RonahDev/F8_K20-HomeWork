45d8737bed4b4b9146c16f8ce8d9505f5376f5f6
RonahDev <kietteve22102005@gmail.com>
2026-05-16 09:38:24 +0700

    practice React

---
+++ ReactJS/demo/hello-react/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return ( //render thứ trong return ra màn hình.
+    <div>
+      <h1>Mai Anh Kiệt</h1> //HTML viết trong javascript
+
+      <p>Front-End và Gym</p>
+
+      <p>22 tuổi</p>
+
+      <button>GitHub</button>
+
+      <button>Youtube</button>
+      
+    </div>
+  )
+}
+
+export default App
