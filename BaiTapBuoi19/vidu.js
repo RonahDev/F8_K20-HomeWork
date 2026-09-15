bbc3f90f7c53ba121bbcf9db560365ded59926bb
RonahDev <kietteve22102005@gmail.com>
2026-09-15 20:29:57 +0700

    learning

---
+++ BaiTapBuoi19/vidu.js
@@ -0,0 +1,12 @@
+/* Argument trong Javascript 
+- Đối số trong js là giá trị thực tế được truyền vào hàm khi gọi hàm đó.
+
+! Có 2 khái niệm cần được làm rõ là Parameter(tham số) và Argument(đối số).
+- Parameter Tên biến được định nghĩa ở phần khai báo hàm
+(đóng vai trò như chỗ trống chờ nhận dữ liệu).
+
+- Argument Giá trị dữ liệu cụ thể truyền vào để 
+lấp đầy các chỗ trống đó khi gọi hàm.
+
+...args : có bao nhiêu đối số truyền vào là sẽ lấy hết 
+*/
